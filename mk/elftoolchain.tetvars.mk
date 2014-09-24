b9dd1df058203a7e89588e4b4ac22cc2ca01b478
Will Andrews <will@firepipe.net>
2014-09-24 07:46:40 -0600

    Make it easier to download TET.

--- mk/elftoolchain.tetvars.mk
+++ mk/elftoolchain.tetvars.mk
@@ -7,11 +7,13 @@
 .include "${SRCTOP}/mk/elftoolchain.base.mk"
 
 # Set TET_ROOT and version.
-TET_VERSION?=		3.8
-TET_ROOT?=		${SRCTOP}/test/tet/tet${TET_VERSION}
+TET_VERSION?=	3.8
+TET_ROOT?=	${OBJTOP}/test/tet/tet${TET_VERSION}
 
-TET_DOWNLOAD_URL=	\
-	http://tetworks.opengroup.org/downloads/38/software/Sources/${TET_VERSION}/tet${TET_VERSION}-src.tar.gz
+TET_SITE=	http://tetworks.opengroup.org/downloads
+TET_VER=	${TET_VERSION:S/.//}
+TET_FILE=	tet${TET_VERSION}-src.tar.gz
+TET_URL=	${TET_SITE}/${TET_VER}/software/Sources/${TET_VERSION}/${TET_FILE}
 
 # The directory where test journals are placed.
 TET_RESULTS_DIR?=	results
